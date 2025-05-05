9a207e8b0b593abccca309e22e7b915f99a5ac8c
AhmedHaggag720 <AhmedHaggag720@gmail.com>
2025-05-05 14:14:08 +0300

    jitsu

--- server.js
+++ server.js
@@ -219,7 +219,7 @@ function fetchListings(logPrefix = "Auto") {
         return false;
       }
 //hi
-      const matches = listings.filter((item) => containsWord(item,"459848"));
+      const matches = listings.filter((item) => containsWord(item,"Jitsu"));
       const matchLog =
         `${logPrefix} listing match:\n` + JSON.stringify(matches, null, 2);
       logToFile(matchLog);
