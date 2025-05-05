af1833406358a9ecca5082a388ac5afdf601553e
AhmedHaggag720 <AhmedHaggag720@gmail.com>
2025-05-05 14:15:05 +0300

    WORLDPAC

--- server.js
+++ server.js
@@ -219,7 +219,7 @@ function fetchListings(logPrefix = "Auto") {
         return false;
       }
 //hi
-      const matches = listings.filter((item) => containsWord(item,"Jitsu"));
+      const matches = listings.filter((item) => containsWord(item,"WORLDPAC"));
       const matchLog =
         `${logPrefix} listing match:\n` + JSON.stringify(matches, null, 2);
       logToFile(matchLog);
